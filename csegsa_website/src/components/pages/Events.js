82e649259900b90685807fdcd4bd17bc1c7683c9
Himanshu Singh <heman.sam@gmail.com>
2022-03-24 12:58:09 -0500

    Events page + basic styling

--- csegsa_website/src/components/pages/Events.js
+++ csegsa_website/src/components/pages/Events.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
+import FullCalendar from '@fullcalendar/react' ;// must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid' ;// a plugin!
+import interactionPlugin from "@fullcalendar/interaction" ;
+
 const Events = (props) => {
 
     const { username } = useContext(AppContext);
@@ -9,14 +13,31 @@ const Events = (props) => {
         console.log("username in Events: " + username);
     }, [username])
 
-    return (
-        <div className="">
-            This is the Events Page
+    const handleEventClick = (eventInfo) => { // bind with an arrow function
+       alert(eventInfo.event.title) ;
 
-            {username !== '' && 
-            <div>Hello, {username}</div>}
-        </div>
-      );  
+    }
+
+      return (
+          <div className="">
+              This is the Events Page
+
+              {username !== '' &&
+              <div>Hello, {username}</div>}
+
+              <div className="eventCalendar">
+                <FullCalendar
+                  plugins={[ dayGridPlugin, interactionPlugin ]}
+                  eventClick={handleEventClick}
+                  initialView="dayGridMonth"
+                  events={[
+                    { title: 'event 1', start: '2022-03-07', end: '2022-03-10' },
+                    { title: 'event 2', date: '2022-03-09' }
+                  ]}
+                />
+              </div>
+          </div>
+        );
 }
 
 export default Events;
